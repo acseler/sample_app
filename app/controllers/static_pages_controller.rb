97d6cf8ae32c5953357ca77ef98b88ef385950df
acsler <acsler@mail.ru>
2016-05-28 23:15:50 +0300

    Microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,10 @@ class StaticPagesController < ApplicationController
   end
 
   def hello
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,9 @@ User.create!(name: 'Alex',
                activated: true,
                activated_at: Time.zone.now)
 end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(6)
+  users.each { |user| user.microposts.create!(content: content) }
+end
