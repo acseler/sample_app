9c73421852b36cf55c7b3cc21a6bd0938d9f8d99
acsler <acsler@mail.ru>
2016-05-29 23:46:10 +0300

    foggg

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,8 +2,8 @@
 
 class PictureUploader < CarrierWave::Uploader::Base
 
-  include CarrierWave::MiniMagick
-  process resize_to_limit: [400, 400]
+  # include CarrierWave::MiniMagick
+  # process resize_to_limit: [400, 400]
 
   if Rails.env.production?
     storage :file
